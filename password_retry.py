c2f015dc919dade120418d77c8180fcb448e8ec9
Michael Wu <wswutw@gmail.com>
2020-03-08 23:37:49 +0800

    don't print out 0 chance

--- password_retry.py
+++ password_retry.py
@@ -6,13 +6,17 @@
 # 如果不正確 就印出 "密碼錯誤! 還有_次機會!"
 
 password = 'a123456'
-i = 3 #剩餘機會
+i = 3 # 剩餘機會
 while i > 0:
+	i = i - 1
 	pwd = input('請輸入密碼: ')	
 	if pwd == password:	
 		print('登入成功!')
-		break #逃出迴圈
+		break # 逃出迴圈
 	else:
-		i = i - 1
-		print('密碼錯誤! 還有', i,'次機會')
+		print('密碼錯誤!')
+		if i > 0:
+			print('還有', i,'次機會')
+		else: 
+			print('沒有機會嘗試了! 帳號被鎖住了')	
 	
